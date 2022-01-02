4298aca03e6384319be609c5ad494c817ee44b14
Hajjaj Abdeljalil <zeronada@hotmail.fr>
2022-01-02 15:26:41 +0100

    my CV online

---
+++ public/app/controllers/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\controllers;
+
+
+class TestController{
+
+    public function index(){
+
+
+    }
+
+
+
+    public function show( int $id){
+    
+        echo "je suis le post".$id;
+
+    }
+
+}
